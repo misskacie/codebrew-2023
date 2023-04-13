b2df00dc46c1317f5c5ffc28e178c05267588305
misskacie <121023187+misskacie@users.noreply.github.com>
2023-04-14 00:21:12 +1000

    add proper cat petting

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,17 @@
 var counter = 0;
 var progressBar = document.getElementById("bar");
 var countLabel = document.getElementById("count-label");
-function incrementCounter() {
-if (counter < 100) {
-    counter++;
-    var width = (counter / 100) * 100;
-    progressBar.style.width = width + "%";
-    progressBar.innerHTML = width.toFixed(0) + "%";
-}
-}
+const catImage = document.querySelector("img");
+
+
+catImage.addEventListener('mousemove', () => {
+    if (counter < 3000) {
+        counter++;
+        var width = (counter / 3000) * 100;
+        progressBar.style.width = width + "%";
+        progressBar.innerHTML = width.toFixed(0) + "%";
+    }
+});
 
 function goToTodoList() {
     window.location.href = "todolist.html";
