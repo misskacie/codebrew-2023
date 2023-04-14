cc9155f900a3e633fb7905eb6502de8fdbe819ed
misskacie <121023187+misskacie@users.noreply.github.com>
2023-04-14 16:41:21 +1000

    add persistent cathappy meter

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 var catHappiness = 0;
+
 var progressBar = document.getElementById("bar");
+
 var countLabel = document.getElementById("count-label");
+
 const catImage = document.querySelector("img");
+
+var catHappiness = sessionStorage.getItem("catHappiness")
+var width = sessionStorage.getItem("width")
+var width = (catHappiness / 3000) * 100;
+
+progressBar.style.width = width + "%";
+progressBar.innerHTML = width.toFixed(0) + "%";
+
+setInterval(function() {
+    var width = (catHappiness / 3000) * 100;
+    progressBar.style.width = width + "%";
+    progressBar.innerHTML = width.toFixed(0) + "%";
+    if (catHappiness > 0 ){
+    catHappiness--;
+    }
+  },100);
 
 // when the mouse is moved over the cat image (simulating petting the cat) cat happiness is increased
 catImage.addEventListener('mousemove', () => {
@@ -10,14 +29,13 @@ catImage.addEventListener('mousemove', () => {
         var width = (catHappiness / 3000) * 100;
         progressBar.style.width = width + "%";
         progressBar.innerHTML = width.toFixed(0) + "%";
+        sessionStorage.setItem("catHappiness",catHappiness)
+        sessionStorage.setItem("width",width)
     }
 });
+
 // every 1000 milliseconds reduce the cat happiness score by 1 (min 0 max 300)
-setInterval(function() {
-    if (catHappiness > 0 ){
-    catHappiness--;
-    }
-  }, 1000);
+
 
 function goToTodoList() {
     window.location.href = "todolist.html";
