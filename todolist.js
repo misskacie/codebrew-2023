9f5eff2f2bd41c10455a5f154244b34d6f42c3cf
misskacie <121023187+misskacie@users.noreply.github.com>
2023-04-14 21:43:52 +1000

    update todolist js

--- todolist.js
+++ todolist.js
@@ -1,25 +1,34 @@
+if(sessionStorage.getItem('tasks')){
+    document.querySelector('#tasks').innerHTML = sessionStorage.getItem('tasks');
+}
+
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Kindly Enter Task Name!!!!")
     }
-
     else{
         document.querySelector('#tasks').innerHTML += `
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <div class="task">
                 <span id="taskname">
                     ${document.querySelector('#newtask input').value}
                 </span>
                 <button class="delete">
-                    <i class="far fa-trash-alt"></i>
+                    <i class="fa fa-trash"></i>
                 </button>
             </div>
         `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+        document.querySelector('#newtask input').value = '';
+        sessionStorage.setItem('tasks', document.querySelector('#tasks').innerHTML);
     }
 }
+document.querySelector('#tasks').addEventListener('click', function(event){
+    if(event.target && event.target.matches('button.delete')){
+        event.target.parentNode.remove();
+
+        sessionStorage.setItem('tasks', document.querySelector('#tasks').innerHTML);
+        var catHappiness = sessionStorage.getItem("catHappiness")
+        catHappiness = catHappiness + 10
+        sessionStorage.setItem("catHappiness",catHappiness)
+    }
+});
